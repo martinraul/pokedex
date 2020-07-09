b627540f9c0d62fd3e38f9269db58fde9fb28720
martinraul <martinracca@gmail.com>
2020-07-09 19:39:10 -0300

    Add Dependencies, Move buttons at the top

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 const $list = document.querySelector("#list");
-$list.style.cursor = "pointer";
 const $nextButton = document.getElementById("next-button");
 const $previousButton = document.getElementById("previous-button");
-let offset = 0;
 const $showPokemon = document.querySelector("#showPokemon");
+
 let pokemonList = `https://pokeapi.co/api/v2/pokemon?offset=0&limit20`;
+let offset = 0;
 
 loadPokemontList(pokemonList);
 
 function loadPokemontList(pokemonList) {
   fetch(pokemonList)
-    .then((data) => data.json())
-    .then((data) => {
-      data.results.forEach((result) => {
+    .then((pokemonList) => pokemonList.json())
+    .then((pokemonList) => {
+      pokemonList.results.forEach((result) => {
         let li = document.createElement("li");
         li.innerHTML = `<li>${result.name}</li>`;
         $list.appendChild(li);
+        $list.style.cursor = "pointer";
         console.log(result.url);
       });
     });
   $list.addEventListener("click", function (event) {
     let pokemon = event.target;
     console.log(pokemon);
-    formatActiveElement(pokemon)
+    formatActiveElement(pokemon);
     loadSinglePokemon(pokemon);
   });
 }
@@ -46,13 +47,13 @@ function createPokemonCard(pokemon) {
   nameSpan.innerHTML = `${pokemon.name}`.toUpperCase();
 
   let typeSpan = document.createElement("p");
-  typeSpan.innerHTML = `Type: ${pokemon.types[0].type.name}`;
+  typeSpan.innerHTML = `Type: ${pokemon.types[0].type.name.toUpperCase()}`;
 
   let heightSpan = document.createElement("p");
-  heightSpan.innerHTML = `height:${pokemon.height}`;
+  heightSpan.innerHTML = `Height: ${pokemon.height}`;
 
   let weightSpan = document.createElement("p");
-  weightSpan.innerHTML = `weight:${pokemon.weight}`;
+  weightSpan.innerHTML = `Weight: ${pokemon.weight}`;
 
   let picSpan = document.createElement("img");
   picSpan.src = Object.values(pokemon.sprites)[4];
@@ -82,7 +83,7 @@ $previousButton.onclick = function () {
   );
 };
 
-function formatActiveElement(pokemon){
+function formatActiveElement(pokemon) {
   var allLiElements = document.getElementsByTagName("li");
   for (i = 0; i < allLiElements.length; i++) {
     //it does work
